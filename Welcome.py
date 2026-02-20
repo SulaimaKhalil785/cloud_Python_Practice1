fc16edb91a3f46da4436fbb9d2321595314698c5
SulaimaKhalil <sulaimakhalil13@gmail.com>
2026-02-20 02:58:54 -0700

    Lesson1: variables

--- Welcome.py
+++ Welcome.py
@@ -1,1 +1,14 @@
 print("Welcome Back")
+
+
+user =" sulaima khalil"
+print(user)
+
+fruit ="Apple"
+print(fruit)
+
+vehicle = "car"
+print(vehicle)
+
+book = "Zindagi jb shuru ho gii"
+print(book)       
